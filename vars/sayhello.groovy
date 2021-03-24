fe0a0e54e9f4428bd88e7e57e819070441b81c5f
Pedireddy Saikiran <71814347+saikiranpedireddy@users.noreply.github.com>
2021-03-24 10:54:53 +0530

    Update sayhello.groovy

--- vars/sayhello.groovy
+++ vars/sayhello.groovy
@@ -1,3 +1,14 @@
-def call(String name) {
-  echo "Hello, ${name} welcome."
+#!/usr/bin/env groovy
+
+def call(String name = 'exist') {
+  def file = new File("/usr/sbin/sa")
+  
+  if (file.exists()){
+    return true;
+  }
+  else {
+    echo " security file not exist."
+  }
+  
+  return false;
 }
