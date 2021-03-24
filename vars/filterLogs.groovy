8327284e3c13da2d52a03f4fc49e342e479cd222
Pedireddy Saikiran <71814347+saikiranpedireddy@users.noreply.github.com>
2021-03-24 11:31:17 +0530

    Update filterLogs.groovy

--- vars/filterLogs.groovy
+++ vars/filterLogs.groovy
@@ -1,8 +1,6 @@
   
 #!/usr/bin/env groovy
 
-import org.apache.commons.lang.StringUtils
-
 def call(String filter_string, int occurrence) {
     def logs = currentBuild.rawBuild.getLog(10000).join('\n')
     int count = StringUtils.countMatches(logs, filter_string);
